f77302367dbceb4462bd0d2111be888fb7a0e203
gray <operasystem6@naver.com>
2021-12-30 01:07:27 +0900

    feat: 회원 도메인 개발

---
+++ src/main/java/com/study/springcorebasic/member/Grade.java
@@ -0,0 +1,6 @@
+package com.study.springcorebasic.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ src/main/java/com/study/springcorebasic/member/MemberService.java
@@ -0,0 +1,8 @@
+package com.study.springcorebasic.member;
+
+public interface MemberService {
+
+    void join(Member member);
+
+    Member findMember(Long memberId);
+}
